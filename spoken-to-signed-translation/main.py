75b22c4969e35e42eabe4ce99493378de9999da9
kamilars <arslanova.25.09.00@gmail.com>
2024-03-25 13:10:32 +0500

    to see which words are not translated

--- spoken-to-signed-translation/main.py
+++ spoken-to-signed-translation/main.py
@@ -11,7 +11,8 @@ def text_to_gif():
     if gif:
         return Response(gif, mimetype='image/gif')
     else:
-        return "Gif cannot be generated because none of the words is in the database", 500
+        msg = "Gif cannot be generated because none of the words is in the database for the words " + text 
+        return msg, 500
     
 if __name__== "__main__":
     app.run()
