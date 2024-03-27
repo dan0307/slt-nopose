1fc9f36a3e4d38e0f01c5d31d61e83d5b70c5281
kamilars <arslanova.25.09.00@gmail.com>
2024-03-28 00:15:22 +0500

    some updates with status code and added perevodit in CSV

--- spoken-to-signed-translation/main.py
+++ spoken-to-signed-translation/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 import functions_framework
 from run import *
 
@@ -7,12 +7,22 @@ app = Flask(__name__)
 @app.route('/')
 def text_to_gif():
     text = request.args.get('text')
+    print(text)
     gif = run_code(text)
     if gif:
-        return Response(gif, mimetype='image/gif')
+        return Response(gif, mimetype='image/gif'), 200
     else:
         msg = "Gif cannot be generated because none of the words is in the database for the words " + text 
-        return msg, 500
+        #with open("no_translation.gif","rb") as f:
+        #    gif_notrans=f.read()
+        #return Response(gif_notrans, mimetype='image/gif'), 418
+        data = {
+            'message': 'No GIF could be created - None of the words could be translated'
+        }
+        
+        # Returning a tuple with response data and status code
+        return jsonify(data), 418
+
     
 if __name__== "__main__":
     app.run()
